bbba7c5b48fb7fdd1e9f03fe60bab4a966fd3b7c
Himanshu <himanshutembhe@gmail.com>
2022-12-20 04:16:43 +0530

    Intial Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,10 +16,10 @@ return(
   <div className="image w-52 h-52"><img id="img" className=" h-52 w-52 " src={img1}alt=""/></div>
   <div className="content flex flex-col flex-wrap items-start justify-start   ">
     <div id="part3" className="details flex flex-col flex-wrap items-start justify-start gap-1  p-1 m-1 ">
-      <div className=" flex flex-col flex-wrap items-start justify-start"  >  <h1  id="head" className=" text-2xl mt-2 font-extrabold ">STUDENTS GYMKHANA</h1>
+      <div className=" flex flex-col flex-wrap items-start justify-start"  ><div id="heading" className=" heading flex flex-row gap-1"><h1  id="head" className=" text-2xl mt-2 font-extrabold ">STUDENTS</h1> <h1 id="head" className=" text-2xl mt-2 font-extrabold " > GYMKHANA</h1></div> 
          <h1 id="head" className="text-2xl ml-0 font-extrabold ">COUNCIL</h1></div> 
         <p align="left" id="one" className="  text-sm ">New SAC Building IIT Guwahati,<br/> Guwahati, Assam - 781039</p>
-        <p align="left" id="one" className="text-sm">+91 -361-258162<br/>sgc@iitg.ac.in</p>
+        <p align="left" id="two" className="text-sm">+91 -361-258162<br/>sgc@iitg.ac.in</p>
     </div>
      <div id="small" className="flex flex-row p-3 ml-4 mt-0 relative right-6 ">
         <img id="img1" className="h-8 w-8" src={img2} alt=""/>
@@ -32,11 +32,11 @@ return(
       <div id="part2" className=" flex flex-col mt-14  gap-2">
         <div id="para" className="flex flex-row gap-2 ">
             <img id="img3" className="h-16 w-16" src={img6} alt=""></img>
-        <p align="left" id="one" className="text-sm content-start ">Maintained by Students<br/>Web committee<br/>IIT Guwahati</p>
+        <p align="left" id="three" className="text-sm content-start ">Maintained by Students<br/>Web committee<br/>IIT Guwahati</p>
         </div>
-        <div className="element2 flex flex-row">
-             <img src={img7} />
-            <p id="one" className="text-sm">Students Gymkhana council, IIT Guwahati</p>
+        <div id="last" className="element2 flex flex-row">
+             <img id="imglast" src={img7} />
+            <p id="four" className="text-sm">Students Gymkhana council, IIT Guwahati</p>
         </div>
       </div>
   </div>
