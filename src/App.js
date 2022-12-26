45d1041e376fcf70efe4e00407996599dda7ef5d
probors <pranshu.cl21@iitg.ac.in>
2022-12-27 04:19:24 +0530

    contactus

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import Navbar from "./components/navbar";
+
 import AquaticsClub from "./pages/aquatics_club";
 import BasketballClub from "./pages/basketballClub";
 import ClubPages from "./pages/clubsPages";
 import Homepage from "./pages/homepage";
 import { Route , Routes} from "react-router-dom";
+import Contactus from "./pages/contactus";
 function App() {
   return (
+
+
+
+    
    <div className="App">
      <Routes>
    <Route   path='/' element={<Homepage />} />
         <Route   path='/clubs' element={< ClubPages/>} />
-        <Route   path='/events'  />
+        <Route   path='/contacts' element={<Contactus />} />
           <Route path='/clubs/basketball' element={<BasketballClub />} />
           <Route path='/basketball' element={<AquaticsClub />} />
    </Routes>
