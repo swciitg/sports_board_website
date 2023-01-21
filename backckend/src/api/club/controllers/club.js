3896a65f81752736bcf2f5c906288a599019b065
mohit <mohitsharma55337@gmail.com>
2023-01-21 14:57:59 +0530

    strapi

--- backckend/src/api/club/controllers/club.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * club controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::club.club');
--- backckend/src/api/club/routes/club.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * club router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::club.club');
