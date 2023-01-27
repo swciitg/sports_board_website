5ae735b65579e1bf0bd52c453de8d6188ef12d8c
Himanshu <himanshutembhe@gmail.com>
2023-01-27 20:24:05 +0530

    Added Events Page

---
+++ src/pages/eventspage.js
@@ -0,0 +1,39 @@
+import Clubs from "../components/clubs";
+import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
+import info from "../components/info"
+import Animation from "../components/animation";
+import Clubone from "../components/clubone";
+import { useEffect, useState } from "react";
+import Eventone from "../components/eventone";
+const EventPages = () => {
+  const [ data, setData]=useState();
+  
+  async function getData (){
+    const api = `http://localhost:1337/api/alleventdata`;
+    const result =await fetch(api);
+    const getResult = await result.json();
+    setData(getResult.data[0]);
+  }
+    useEffect(()=>{
+      getData();
+  },[])
+ 
+  console.log(data);
+   return (
+       <div>
+           {/* <Navbar
+            name = {info[1].name}
+            info = {info[1].info}
+          />
+           <Animation/> 
+          <Footer/>  */}
+         <Eventone  data={data}></Eventone>
+
+       </div>
+
+
+        );  
+
+}
+export default EventPages;
