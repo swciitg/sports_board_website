689f398f6ca3d97a4fa7e4b4db1d741baaec30b2
Himanshu <himanshutembhe@gmail.com>
2023-02-13 07:01:26 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ function Footer(props)
   }
 return(
 
-<div id="containerone"  className="  flex flex-row gap-64 w-100% h-80 mt-0 pl-0 px-0  " style={{position:'relative' ,zIndex:'1',backgroundColor: text }} >
+<div id="containerone"  className="  flex flex-row gap-64 w-100% h-80 mt-0 pl-0 px-0 mb-0  " style={{position:'relative',zIndex:'1',backgroundColor: text }} >
 <div id="part1" className="flex flex-row ml-44 gap-x-6 ">
   <div className="image w-52 h-52 "><img id="img" className=" h-52 w-52 " src={img1}alt=""/></div>
   <div className="content flex flex-col flex-wrap items-start justify-start   ">
