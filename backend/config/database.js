9ac586f790fc3dade61e24382954263d1b56fbe3
Arpit Sureka <surekaarpit@gmail.com>
2023-02-19 02:43:19 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,9 +9,6 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'bank'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '0000'),
-      ssl: {
-        rejectUnauthorized: false,
-      },
     },
     debug: false,
   },
