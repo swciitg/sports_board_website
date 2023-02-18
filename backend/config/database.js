6a22baea520be1419c6d37d53f4fdfa4d764db1d
Arpit Sureka <surekaarpit@gmail.com>
2023-02-19 02:56:58 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,21 +1,18 @@
-const path = require('path');
+// path: ./config/database.js
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'bank'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '0000'),
-      ssl: env('DATABASE_SSL', false)
-    },
-    options: {
-      host: 'swc.iitg.ac.in',
-      port: 8188,
-      path: ''
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env('DATABASE_SSL', false),
     },
     debug: false,
   },
 });
+ 
