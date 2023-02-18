d417c884261ccf9b6bcdb5ab42dc969061f8e311
Arpit Sureka <surekaarpit@gmail.com>
2023-02-19 02:52:36 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,6 +9,12 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'bank'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '0000'),
+      ssl: env('DATABASE_SSL', false)
+    },
+    options: {
+      host: 'swc.iitg.ac.in',
+      port: 8188,
+      path: ''
     },
     debug: false,
   },
