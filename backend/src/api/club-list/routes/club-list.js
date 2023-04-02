a61386bfb1ec2c2d623a2a815265c124852c5205
swc <web_swc@iitg.ac.in>
2023-04-02 19:34:08 +0000

    fdvs

---
+++ backend/src/api/club-list/routes/club-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * club-list router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::club-list.club-list');
---
+++ backend/src/api/club-list/services/club-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * club-list service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::club-list.club-list');
