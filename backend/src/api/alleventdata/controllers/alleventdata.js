d3f87d9f10438dc91cc0c2c545abcef769d109af
Arpit Sureka <surekaarpit@gmail.com>
2023-04-03 01:37:28 +0530

    Update alleventdata.js

--- backend/src/api/alleventdata/controllers/alleventdata.js
+++ backend/src/api/alleventdata/controllers/alleventdata.js
@@ -7,14 +7,18 @@
 module.exports = {
   exampleAction: async (ctx, next) => {
     try {
-      ctx.body["data"] = await strapi.entityService.findMany('api::event.event', {
+      var data = await strapi.entityService.findMany('api::event.event', {
         populate: ['Thumbnail_Image', 'EventName', 'Thumbnail_Content'],
       });
-      ctx.body["images"] = await strapi.entityService.findOne('api::event-list.event-list', 1, {
+      var images = await strapi.entityService.findOne('api::event-list.event-list', 1, {
         populate: {
           carousel : true,
         },
       });
+      ctx.body = {
+        data : data,
+        images : images
+      }
     } catch (err) {
       ctx.body = err;
     }
