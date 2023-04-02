b34d43a97b32de264990e0fe5e1d2ef73c59cb6e
Arpit Sureka <surekaarpit@gmail.com>
2023-04-03 01:39:29 +0530

    Update clubsdata.js

--- backend/src/api/clubsdata/controllers/clubsdata.js
+++ backend/src/api/clubsdata/controllers/clubsdata.js
@@ -7,14 +7,18 @@
 module.exports = {
   exampleAction: async (ctx, next) => {
     try {
-      ctx.body = await strapi.entityService.findMany('api::club.club', {
+      var data = ctx.body = await strapi.entityService.findMany('api::club.club', {
         populate: ['Thumbnail_Image', 'ClubName', 'Thumbnail_Content'],
       });
-      // ctx.body += await strapi.entityService.findOne('api::club-list.club-list', 1, {
-      //   populate: {
-      //     carousel : true,
-      //   },
-      // });
+      var images = await strapi.entityService.findOne('api::club-list.club-list', 1, {
+        populate: {
+          carousel : true,
+        },
+      });
+      ctx.body = {
+        data : data,
+        images : images
+      }
     } catch (err) {
       ctx.body = err;
     }
